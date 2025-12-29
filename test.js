80078982092e9c9cbfdb6e25bc62b84521d2210c
mazher0203 <m.azher0203@gmail.com>
2025-12-29 16:01:54 +0530

    Initial project setup with CI/CD

---
+++ test.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when value not present', function() {
+      assert.equal([1,2,3].indexOf(4), -1);
+    });
+  });
+});
